bcb94177bc410ae445836fd9ab2a6fa42e9da14b
paurbano <paurbano@gmail.com>
2020-01-06 04:31:59 +0000

    math operations

--- opcodes1.c
+++ opcodes1.c
@@ -34,3 +34,72 @@ void nop(stack_t **stack, unsigned int line)
 	(void)stack;
 	(void)line;
 }
+
+/**
+ * sub - subtracts top element from the second top element of the stack.
+ * @stack: pointer to the head node pointer of stack
+ * @line: the line number
+ * Return: Nothing.
+ */
+void sub(stack_t **stack, unsigned int line)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n -= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
+
+/**
+ * div - divide top element from the second top element of the stack.
+ * @stack: pointer to the head node pointer of stack
+ * @line: the line number
+ * Return: Nothing.
+ */
+void div(stack_t **stack, unsigned int line)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n /= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
+
+/**
+ * mul - multiplies second top element with the top element of the stack.
+ * @stack: pointer to the head node pointer of stack
+ * @line: the line number
+ * Return: Nothing.
+ */
+void mul(stack_t **stack, unsigned int line)
+{
+	stack_t *temp;
+
+	if (!(*stack) || !(*stack)->next)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", line);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	(*stack)->next->n *= (*stack)->n;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
+}
